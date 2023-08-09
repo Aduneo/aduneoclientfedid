8625d8c3f2fbc550e9c76f1cca9f1251476c1cff
Hanna Nassar <hannanassar@hotmail.com>
2023-08-09 17:21:41 +0200

    Conf.py fixed project information

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'AduneoClientFedID'
+copyright = 'Aduneo'
+author = 'Aduneo'
 
 release = '0.1'
 version = '0.1.0'
