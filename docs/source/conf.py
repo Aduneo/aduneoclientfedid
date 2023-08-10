6231fed73d7abe7fbb151a3480532c63814cad1b
Hanna Nassar <hannanassar@hotmail.com>
2023-08-10 09:24:55 +0200

    readthedocs conf.py adjusted theme

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,12 +2,12 @@
 
 # -- Project information
 
-project = 'AduneoClientFedID'
+project = 'aduneoclientfedid'
 copyright = 'Aduneo'
 author = 'Aduneo'
 
 release = '0.1'
-version = '0.1.0'
+version = '1.0.2'
 
 # -- General configuration
 
@@ -29,7 +29,7 @@ templates_path = ['_templates']
 
 # -- Options for HTML output
 
-html_theme = 'sphinx_rtd_theme'
+html_theme = 'classic'
 
 # -- Options for EPUB output
 epub_show_urls = 'footnote'
